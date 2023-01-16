d9dbfebec02324d820ba5f4671ebfbd05679baed
Baptiste Fouquart <49322183+BaptisteFouquart@users.noreply.github.com>
2023-01-16 15:47:48 +0100

    Update keys.py

--- actions/keys.py
+++ actions/keys.py
@@ -1,4 +1,3 @@
 AMAZON_API_KEY = ... # API key for amazon Alexa Skill
-APOD_API_KEY = ... # API key for NASA Picture of the day
+APOD_API_KEY = 'yXXcq7UbFjvxYObV4ogigt5TznDpzjJC3ujf9VkW' # API key for NASA Picture of the day
 GEO_API_KEY = ... # API key for Geography API
- 
