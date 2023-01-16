ded08f716c3b5f08744f3648b9397a772dc03424
BastianMuratory <bastian.muratory@gmail.com>
2023-01-16 15:44:52 +0100

    Add files for main action

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,49 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
+# This files contains a custom actions which is used 
+# to run the correct function according to the user intent.
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+from typing import Any, Text, Dict, List
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionAPI(Action):
+
+    iter = 0
+
+    #geoAPI = ...
+    #triviaAPI = ...
+    #nasaAPI = ...
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.iter = 0
+        #self.geoAPI = ...
+        #self.triviaAPI = ...
+        #self.nasaAPI = ...
+
+    def name(self) -> Text:
+        return "action_API"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        intent = tracker.latest_message['intent'].get('name')
+
+        print(f"\nintent = {intent}\n")
+
+        if intent == "geo" :
+            pass
+        elif intent == "trivia" :
+            pass
+        elif intent == "nasa" :
+            pass
+
+
+        self.iter += 1
+        dispatcher.utter_message(text=f"Hello World! n°{self.iter}")
+
+        return []
+
