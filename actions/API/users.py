964a4b90284091891edfda8be17151c75527cc32
Antoine Paulus <antoine.paulus@univ-tlse3.fr>
2023-01-17 17:28:52 +0100

    user.py  for games and scores

---
+++ actions/API/users.py
@@ -0,0 +1,47 @@
+import json
+from enum import Enum
+
+class Game (Enum):
+    TRIVIA = "trivia_score"
+    GEO = "geo_score"
+
+
+def user_get_score(user_name : str, game_name : Game) -> int: 
+    """Return the score of a player for a given game. Please use game enum from the import"""
+    file = open("data/user_data.json",'r')
+    data = json.load(file)
+    file.close()
+    if user_name in data.keys():
+        return data[user_name][game_name.value]
+    return 0
+    
+def increment_user_score(user_name : str, game_name : Game, points : int = 1):
+    """Update the score of a user for a given game"""
+    file = open("data/user_data.json",'r')
+    data = json.load(file)
+    file.close()
+    if user_name not in data.keys():
+        # This user doesn't exist. We create one
+        data[user_name] = {"trivia_score" : 0, "geo_score" : 0, "incoming_chats" : []}
+    data[user_name][game_name.value] += points
+    file = open("data/user_data.json",'w')
+    json.dump(data,file)
+    file.close()
+
+def get_user_data(user_name : str) -> dict : 
+    file = open("data/user_data.json",'r')
+    data = json.load(file)
+    file.close()
+    if user_name not in data.keys():
+        # This user doesn't exist. We create one
+        data[user_name] = {"trivia_score" : 0, "geo_score" : 0, "incoming_chats" : []}
+        file = open("data/user_data.json",'w')
+        json.dump(data,file)
+        file.close()
+    return data[user_name]
+
+if __name__ == "__main__" : 
+    print(user_get_score("jane",Game.TRIVIA))
+    increment_user_score("jane",Game.TRIVIA,3)
+    print(user_get_score("jane",Game.TRIVIA))
+
